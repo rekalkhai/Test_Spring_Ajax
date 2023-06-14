b6979340dc475918673e8361d581463a07b96e93
rekalkhai <maihoangkhaitien@gmail.com>
2023-06-14 17:41:44 +0700

    ok

---
+++ src/main/java/rekal/controller/ViewController.java
@@ -0,0 +1,13 @@
+package rekal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
---
+++ src/test/java/rekal/SpringAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package rekal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
